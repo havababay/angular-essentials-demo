916e5dc029a11b71c97ec86bd9eb71652015e16c
havababay <hava.babay@gmail.com>
2024-01-22 21:03:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Person } from './../shared/model/person';
+import { Component} from '@angular/core';
+import { BindingComponent } from './binding/binding.component';
+import { CommunicationComponent } from './communication/communication.component';
+import { NgforDemoComponent } from './ngfor-demo/ngfor-demo.component';
+import { NgifDemoComponent } from './ngif-demo/ngif-demo.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [BindingComponent, CommunicationComponent, NgforDemoComponent, NgifDemoComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo4';
+
+  isChildButtonEnabled = false;
+
+  printLightStatus(status : boolean) {
+    console.log("Is the light on? " + status);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { BindingComponent } from './binding/binding.component';
+import { CommunicationComponent } from './communication/communication.component';
+import { NgifDemoComponent } from './ngif-demo/ngif-demo.component';
+import { NgforDemoComponent } from './ngfor-demo/ngfor-demo.component';
+import { HomeDemoComponent } from './home-demo/home-demo.component';
+import { RouterDemoComponent } from './router-demo/router-demo.component';
+import { FormsDemoComponent } from './forms-demo/forms-demo.component';
+import { FormsDemoFruitsComponent } from './forms-demo-fruits/forms-demo-fruits.component';
+import { PersonListComponent } from './person-list/person-list.component';
+import { ErrorHandlingDemoComponent } from './error-handling-demo/error-handling-demo.component';
+import { PersonFormComponent } from './person-form/person-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'binding', component: BindingComponent},
+    {path: 'communication', component: CommunicationComponent},
+    {path: 'ngif', component: NgifDemoComponent},
+    {path: 'ngfor', component: NgforDemoComponent},
+    {path: '', component: HomeDemoComponent},
+    {path: 'routerdemo/:id', component: RouterDemoComponent},
+    {path: 'person/:stringId', component: PersonFormComponent},
+    {path: 'newperson', component: PersonFormComponent},
+    {path: 'persons', component: PersonListComponent},
+    {path: 'fruitsform', component: FormsDemoFruitsComponent},
+    {path: 'errors', component: ErrorHandlingDemoComponent},
+];
